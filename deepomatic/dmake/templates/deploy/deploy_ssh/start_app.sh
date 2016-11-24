bb13124f5c5c945445f7c7632c9c8adfb342ea3e
Vincent Delaitre <vincent.delaitre@gmail.com>
2016-11-24 18:57:37 +0100

    remove app mounting when deploying via ssh

--- deepomatic/dmake/templates/deploy/deploy_ssh/start_app.sh
+++ deepomatic/dmake/templates/deploy/deploy_ssh/start_app.sh
@@ -46,7 +46,7 @@ docker pull ${IMAGE_NAME}
 RUN_COMMAND="docker run --privileged ${DOCKER_OPTS} -v /var/log:/var/log"
 # Options to share docker (if the hooks wants to launch another container)
 DOCKER_SHARE_OPTS="-v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/usr/bin/docker -v /usr/lib/x86_64-linux-gnu/libltdl.so.7:/usr/lib/x86_64-linux-gnu/libltdl.so.7"
-RUN_COMMAND_HOOKS="$RUN_COMMAND --rm $DOCKER_SHARE_OPTS -v $PWD:/app -t -i ${IMAGE_NAME}"
+RUN_COMMAND_HOOKS="$RUN_COMMAND --rm $DOCKER_SHARE_OPTS -t -i ${IMAGE_NAME}"
 # Run pre hooks
 if [ ! -z "${PRE_DEPLOY_HOOKS}" ]; then
     echo "Running pre-deploy script ${PRE_DEPLOY_HOOKS}"
