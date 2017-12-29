Deepomatic/dmake

commit 9617bb01fd073cba001c181f3af70273b4489820

Evaluate needed service env in context (#104)
* Evaluate needed service env in context

* remove commented code

deepomatic/dmake/deepobuild.py
@@ -792,12 +792,6 @@ class NeededServiceSerializer(YAML2PipelineSerializer):
         super(NeededServiceSerializer, self).__init__(**kwargs)
         self._specialized = True
 
-    def __str__(self):
-        s = "%s" % (self.service_name)
-        if self._specialized:
-            s += " -- env: %s" % (self.env)
-        return s
-
     def __eq__(self, other):
         # NeededServiceSerializer objects are equal if equivalent, to deduplicate their instances at runtime
         # objects are not comparable before the call to _validate_(), because fields are not populated yet
@@ -815,12 +809,15 @@ class NeededServiceSerializer(YAML2PipelineSerializer):
         # also accept simple variant where data is a string: the service_name
         if common.is_string(data):
             data = {'service_name': data}
-            self._specialized = False
-        return super(NeededServiceSerializer, self)._validate_(file, needed_migrations=needed_migrations, data=data, field_name=field_name)
+        result = super(NeededServiceSerializer, self)._validate_(file, needed_migrations=needed_migrations, data=data, field_name=field_name)
+        self._specialized = len(self.env) > 0
+        return result
 
-    def populate_env(self, context_env):
+    def get_env(self, context_env):
+        contextualized_env = {}
         for key, value in self.env.items():
-            self.env[key] = common.eval_str_in_env(value, context_env)
+            contextualized_env[key] = common.eval_str_in_env(value, context_env)
+        return contextualized_env
 
     def get_service_name_unique_suffix(self):
         return "--%s" % id(self) if self._specialized else ""
@@ -907,12 +904,6 @@ class DMakeFile(DMakeFileSerializer):
                     env.__fields__['source'].value = None
                 self.__fields__['env'] = env
 
-        # populate needed_services environment
-        runtime_env = self.__fields__['env'].get_replaced_variables()
-        for service in self.__fields__['services'].value:
-            for needed_service in service.needed_services:
-                needed_service.populate_env(runtime_env)
-
         self.docker_services_image = None
         self.app_package_dirs = {}
 
@@ -995,16 +986,16 @@ class DMakeFile(DMakeFileSerializer):
         if not service.config.has_value() or not service.config.docker_image.has_value() or service.config.docker_image.start_script is None:
             return
 
+        context_env = self.env.get_replaced_variables(docker_links=docker_links, needed_links=service.needed_links)
         unique_service_name = service_name
+
         customized_env = {}
         if service_customization:
-            customized_env = service_customization.env
+            customized_env = service_customization.get_env(context_env)
             # daemon name: <app_name>/<service_name><optional_unique_suffix>; service_name already contains "<app_name>/"
             unique_service_name += service_customization.get_service_name_unique_suffix()
-
         opts = self._launch_options_(commands, service, docker_links, customized_env, run_base_image=False, mount_root_dir=False)
-        env = self.env.get_replaced_variables(docker_links=docker_links, needed_links=service.needed_links)
-        image_name = service.config.docker_image.get_image_name(service_name, env)
+        image_name = service.config.docker_image.get_image_name(service_name, context_env)
 
         # <DEPRECATED>
         if service.config.pre_deploy_script:

commit da21a14c767bccb04eac5dcd09f64c486f49912d

Refactor generate_run and generate_test: they both start the service image
Slight behavior change: service docker image name is not evaluated in
the needed_service parent context anymore. It was an error introduced
by PR #104 (9617bb01fd073cba001c181f3af70273b4489820) that this
refactor revealed: service image name is constant for a given service,
it should not depend on how its parent services uses it.

deepomatic/dmake/deepobuild.py
@@ -838,6 +838,14 @@ class TestSerializer(YAML2PipelineSerializer):
     cobertura_report   = FieldSerializer("string", optional = True, example = "**/coverage.xml", help_text = "Publish a Cobertura report. **WARNING** only one is allowed per repository.")
     html_report        = HTMLReportSerializer(optional = True, help_text = "Publish an HTML report.")
 
+    def get_mounts_opt(self, service_name):
+        if not self.has_value():
+            return ''
+        opts = []
+        for data_volume in self.data_volumes:
+            opts.append(data_volume.get_mount_opt(service_name))
+        return ' ' + ' '.join(opts)
+
     def generate_test(self, commands, path, service_name, docker_cmd, docker_links, mount_point):
         if not self.has_value() or len(self.commands) == 0:
             return
@@ -1111,6 +1119,16 @@ class DMakeFile(DMakeFileSerializer):
         base_image = self.docker.get_base_image_from_service_name(base_image_service_name)
         base_image._serialize_(commands, self.__path__)
 
+    def _generate_run_docker_opts_(self, commands, service, docker_links, env=None):
+        if env is None:
+            env = {}
+        docker_opts = self._launch_options_(commands, service, docker_links, env=env, run_base_image=False, mount_root_dir=False)
+
+        env = self.env.get_replaced_variables(docker_links=docker_links, needed_links=service.needed_links)
+        image_name = service.config.docker_image.get_image_name(env=env)
+
+        return docker_opts, image_name
+
     def generate_run(self, commands, service_name, docker_links, service_customization):
         service = self._get_service_(service_name)
         if not service.config.has_value() or not service.config.docker_image.has_value() or service.config.docker_image.start_script is None:
@@ -1124,11 +1142,14 @@ class DMakeFile(DMakeFileSerializer):
             customized_env = service_customization.get_env(context_env)
             # daemon name: <app_name>/<service_name><optional_unique_suffix>; service_name already contains "<app_name>/"
             unique_service_name += service_customization.get_service_name_unique_suffix()
-        opts = self._launch_options_(commands, service, docker_links, customized_env, run_base_image=False, mount_root_dir=False)
-        image_name = service.config.docker_image.get_image_name(env=context_env)
 
-        append_command(commands, 'read_sh', var = "DAEMON_ID", shell = 'dmake_run_docker_daemon "%s" "" %s -i %s' % (unique_service_name, opts, image_name))
+        docker_opts, image_name = self._generate_run_docker_opts_(commands, service, docker_links, customized_env)
+        docker_cmd = 'dmake_run_docker_daemon "%s" "" %s -i %s' % (unique_service_name, docker_opts, image_name)
 
+        # Run daemon
+        append_command(commands, 'read_sh', var = "DAEMON_ID", shell = docker_cmd)
+
+        # Wait for daemon to be ready
         cmd = service.config.readiness_probe.get_cmd()
         if cmd:
             append_command(commands, 'sh', shell = 'dmake_exec_docker "$DAEMON_ID" %s' % cmd)
@@ -1160,38 +1181,17 @@ class DMakeFile(DMakeFileSerializer):
 
         return docker_opts
 
-    def _generate_shell_docker_cmd_(self, commands, service, service_name, docker_links):
-        docker_opts  = self._launch_options_(commands, service, docker_links, self.build.env, run_base_image=True, mount_root_dir=True)
+    def generate_shell(self, commands, service_name, docker_links):
+        service = self._get_service_(service_name)
 
-        if service.tests.has_value():
-            opts = []
-            for data_volume in service.tests.data_volumes:
-                opts.append(data_volume.get_mount_opt(service_name))
-            docker_opts += " " + (" ".join(opts))
+        docker_opts = self._launch_options_(commands, service, docker_links, self.build.env, run_base_image=True, mount_root_dir=True)
+        docker_opts += service.tests.get_mounts_opt(service_name)
 
         docker_base_image = self.docker.get_docker_base_image(service.get_base_image_variant())
         docker_opts += " -i %s" % docker_base_image
 
-        return "dmake_run_docker_command %s " % docker_opts
+        docker_cmd = "dmake_run_docker_command %s " % docker_opts
 
-    def _generate_test_docker_cmd_(self, commands, service, service_name, docker_links):
-        docker_opts  = self._launch_options_(commands, service, docker_links, env=None, run_base_image=False, mount_root_dir=False)
-
-        if service.tests.has_value():
-            opts = []
-            for data_volume in service.tests.data_volumes:
-                opts.append(data_volume.get_mount_opt(service_name))
-            docker_opts += " " + (" ".join(opts))
-
-        env = self.env.get_replaced_variables(docker_links=docker_links, needed_links=service.needed_links)
-        image_name = service.config.docker_image.get_image_name(env = env)
-        docker_opts += " -i %s" % image_name
-
-        return 'dmake_run_docker_test %s "" %s ' % (service_name, docker_opts)
-
-    def generate_shell(self, commands, service_name, docker_links):
-        service = self._get_service_(service_name)
-        docker_cmd = self._generate_shell_docker_cmd_(commands, service, service_name, docker_links)
         append_command(commands, 'sh', shell = docker_cmd + self.docker.command)
 
     def generate_test(self, commands, service_name, docker_links):
@@ -1200,7 +1200,10 @@ class DMakeFile(DMakeFileSerializer):
             return
         if not service.config.docker_image.has_value():
             raise DMakeException("You need to specify a 'config.docker_image' when testing.")
-        docker_cmd = self._generate_test_docker_cmd_(commands, service, service_name, docker_links)
+
+        docker_opts, image_name = self._generate_run_docker_opts_(commands, service, docker_links)
+        docker_opts += service.tests.get_mounts_opt(service_name)
+        docker_cmd = 'dmake_run_docker_test %s "" %s -i %s ' % (service_name, docker_opts, image_name)
 
         # Run test commands
         service.tests.generate_test(commands, self.__path__, service_name, docker_cmd, docker_links, self.docker.mount_point)