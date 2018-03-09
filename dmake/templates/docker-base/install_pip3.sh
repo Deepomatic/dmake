dee7b03a9e857f9bc571f7f285418cd31e67664f
Vincent Delaitre <vincent@deepomatic.com>
2018-03-09 11:46:14 +0100

    Set python3 as a low-priority alternative when using python3_requirements

--- dmake/templates/docker-base/install_pip3.sh
+++ dmake/templates/docker-base/install_pip3.sh
@@ -9,6 +9,8 @@
 if [ `which pip3 | wc -l` = "0" ]; then
     echo "N" | apt-get --no-install-recommends -y install openssh-client
     apt-get --no-install-recommends -y install python3-setuptools python3-dev libffi-dev libssl-dev curl g++
+    # Sets python3 as a python alternative with a low priority
+    update-alternatives --install /usr/bin/python python /usr/bin/python3 0
     curl https://bootstrap.pypa.io/get-pip.py | python3
     pip3 install --upgrade pip
 fi
