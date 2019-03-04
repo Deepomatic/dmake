Deepomatic/dmake

commit a2230e5a97b9bb8ae7aedbb9cd96615e0a09be0a

Better stage name in execution plan (#346)
The `test` stage executes containers (except for the shell which is in
`deploy`): let's call it `Running App` instead of `Testing App` which
implies there are tests.

dmake/core.py
@@ -774,7 +774,7 @@ def make(options, parse_files_only=False):
 
         ordered_build_files = [('Building Base', base),
                                ('Building App', build),
-                               ('Testing App', test)]
+                               ('Running App', test)]
 
         if not common.is_pr:
             ordered_build_files.append(('Deploying', list(deploy)))

commit b185557e454dea2b83e7c63d0113e7dbebec28c0

Fix Jenkins GPU resource reservation
It was accidentally broken by #346.

dmake/core.py
@@ -847,7 +847,7 @@ def make(options, parse_files_only=False):
 
         # GPU resource lock
         # `common.need_gpu` is set during Testing commands generations: need to delay adding commands to all_commands to create the gpu lock if needed around the Testing stage
-        lock_gpu = stage == "Testing App" and common.need_gpu
+        lock_gpu = (stage == "Running App") and common.need_gpu
         if lock_gpu:
             append_command(all_commands, 'lock', label='GPUS')
 