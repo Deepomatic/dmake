1df5824fb7b0a044e1cb894d17ab866a33b55762
Thomas Riccardi <thomas@deepomatic.com>
2021-01-25 20:00:23 +0100

    Pin bootstrap.pypa.io pip installer for specific python versions, the default one now requires python 3.6+ (#477)
    Later: drop these features

--- dmake/templates/docker-base/install_pip3.sh
+++ dmake/templates/docker-base/install_pip3.sh
@@ -9,5 +9,5 @@
 if [ `which pip3 | wc -l` = "0" ]; then
     echo "N" | apt-get --no-install-recommends -y install openssh-client
     apt-get --no-install-recommends -y install python3-setuptools python3-dev libffi-dev libssl-dev curl g++
-    curl https://bootstrap.pypa.io/get-pip.py | python3 - pip==9.0.3
+    curl https://bootstrap.pypa.io/3.5/get-pip.py | python3 - pip==9.0.3
 fi
