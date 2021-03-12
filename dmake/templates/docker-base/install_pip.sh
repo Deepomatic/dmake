56637e075a1e80437301cb1ecab8baa7b81bc662
Thomas Riccardi <thomas@deepomatic.com>
2021-03-12 12:14:40 +0100

    Fix curl get-pip that they broke again... (#488)

--- dmake/templates/docker-base/install_pip.sh
+++ dmake/templates/docker-base/install_pip.sh
@@ -9,5 +9,5 @@
 if [ `which pip | wc -l` = "0" ]; then
     echo "N" | apt-get --no-install-recommends -y install openssh-client
     apt-get --no-install-recommends -y install python-setuptools python-dev libffi-dev libssl-dev curl g++
-    curl https://bootstrap.pypa.io/2.7/get-pip.py | python - pip==9.0.3
+    curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python - pip==9.0.3
 fi
