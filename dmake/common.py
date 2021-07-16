Deepomatic/dmake

commit c55d038c0dba956ba300e2074b6a20c71e165e87

Properly handle errors in git get upstream remote (#343)
This avoids infinite loop when recursing on error.

Also disambiguate between rev and filename.

dmake/common.py
@@ -171,7 +171,10 @@ def find_repo_root(path=os.getcwd()):
     return root_dir, sub_dir
 
 def git_get_upstream_branch_remote(branch):
-    upstream_branch = run_shell_command('git rev-parse --abbrev-ref --symbolic-full-name {}@{{upstream}}'.format(branch), ignore_error=True)
+    try:
+        upstream_branch = run_shell_command('git rev-parse --abbrev-ref --symbolic-full-name {}@{{upstream}} --'.format(branch), raise_on_return_code=True)
+    except ShellError:
+        upstream_branch = None
     if not upstream_branch:
         # assume 'origin' as remote name
         return 'origin'

commit b4021e94b6cea1b8fa06a442eb084127e7f3c710

git rev-parse doesn't read `--` as expected: remove it
Fix regression introduced in c55d038c0dba956ba300e2074b6a20c71e165e87

Somehow on jenkins it still worked, maybe the change detection mode
didn't work well locally, but it's not really used.

dmake/common.py
@@ -285,7 +285,7 @@ def find_repo_root(path=os.getcwd()):
 
 def git_get_upstream_branch_remote(branch):
     try:
-        upstream_branch = run_shell_command('git rev-parse --abbrev-ref --symbolic-full-name {}@{{upstream}} --'.format(branch), raise_on_return_code=True)
+        upstream_branch = run_shell_command('git rev-parse --abbrev-ref --symbolic-full-name {}@{{upstream}}'.format(branch), raise_on_return_code=True)
     except ShellError:
         upstream_branch = None
     if not upstream_branch: