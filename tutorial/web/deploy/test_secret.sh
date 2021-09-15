269d27c6ac1c573b3ff017752c317d3c11af4709
Julien CHATY-CAPELLE <julien.chaty-capelle@deepomatic.com>
2021-09-15 19:50:15 +0200

    fixed test secret path

--- tutorial/web/deploy/test_secret.sh
+++ tutorial/web/deploy/test_secret.sh
@@ -1,4 +1,4 @@
 # Script will fail on any error
 set -e
 
-/bin/bash -c 'v=$(cat /run/secret/secretvalue); if [ "$v" == "this_is_a_secret_path" ]; then exit 0; else exit 1; fi;'
+/bin/bash -c 'v=$(cat /run/secrets/secretvalue); if [ "$v" == "this_is_a_secret_path" ]; then exit 0; else exit 1; fi;'
