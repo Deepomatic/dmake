dd7a2b52f84e1ce8d0f6a7446b2632f6ef3fc59c
Vincent Delaitre <vincent@deepomatic.com>
2025-01-07 15:55:32 +0100

    Remove try/catch

--- dmake/core.py
+++ dmake/core.py
@@ -665,14 +665,8 @@ def generate_command_pipeline(file, cmds):
     indent_level += 1
 
     if emit_cobertura:
-        write_line("try {")
-        indent_level += 1
         write_line("recordCoverage(tools: [[parser: 'COBERTURA', pattern: '%s/**/*.xml']], qualityGates: [[metric: 'LINE', baseline: 'PROJECT_DELTA', criticality: 'NOTE']], sourceCodeRetention: 'NEVER')" % (cobertura_tests_results_dir))
         write_line('''sh('rm -rf "%s"')''' % cobertura_tests_results_dir)
-        indent_level -= 1
-        write_line("} catch (error) {")
-        write_line("  dmake_echo 'Late cobertura_report test result collection failed, it may be because the test steps were not reached (earlier error: check logs/steps above/before), or because the cobertura_report is misconfigured (check the path config). Error message: ${error}'")
-        write_line("}")
 
     write_line('sh("dmake_clean")')
     indent_level -= 1
